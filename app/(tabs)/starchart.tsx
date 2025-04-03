f7ad0912184c76815bd75bc236c3b156d84ebe8d
mglaum <maddieglaum@gmail.com>
2025-04-03 15:54:27 -0400

    fixed starchart bug

--- app/(tabs)/starchart.tsx
+++ app/(tabs)/starchart.tsx
@@ -1,20 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import { View } from "react-native";
-import { WebView } from "react-native-webview";
-import StarMap from "@/components/RenderStarchart";
-import StarChart from "@/components/StarChart";
-// import RenderStarchart 
-import TestWebView from "@/components/TestComponent"; // Fallback for testing in Expo Go
 import StarChartScreen from "@/components/StarchartHeader";
 
 export default function ShowStarChart() {
   return (
     <View style={{ flex: 1 }}>
       <StarChartScreen />
-      {/* <StarChart /> */}
-      {/* <TestWebView/> */}
-      {/* <StarMap/> */}
-      {/* <RenderStarchart /> */}
     </View>
   );
 }
